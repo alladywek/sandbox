38226acc3897f112bceec3c128e2d2a82addc125
Abbas Abbasov <alladywekjd@gmail.com>
2017-10-14 13:36:53 +0300

    Add qsort implementation

---
+++ src/main/kotlin/org/alladywek/qsort/QSort.kt
@@ -0,0 +1,38 @@
+package org.alladywek.qsort
+
+class QSort {
+
+    fun sort(arr: MutableList<Int>, lo: Int = 0, hi: Int = arr.lastIndex) {
+        if (lo < hi) {
+            val p = partition(arr, lo, hi)
+            sort(arr, lo, p - 1)
+            sort(arr, p, hi)
+        }
+    }
+
+    private fun partition(arr: MutableList<Int>, lo: Int, hi: Int): Int {
+        val pivot = getPivot(arr, lo, hi)
+        var i = lo
+        var j = hi
+        while (i <= j) {
+            while (arr[i] < pivot) {
+                i++
+            }
+            while (arr[j] > pivot) {
+                j--
+            }
+            if (i <= j) {
+                val temp = arr[i]
+                arr[i] = arr[j]
+                arr[j] = temp
+                i++
+                j--
+            }
+        }
+        return i
+    }
+
+    private fun getPivot(arr: MutableList<Int>, lo: Int, hi: Int): Int {
+        return ((arr[lo] + arr[hi]) * 0.5).toInt()
+    }
+}
