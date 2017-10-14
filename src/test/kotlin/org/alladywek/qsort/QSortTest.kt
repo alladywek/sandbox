cf7392876c9d7f3c645bef86f1753d28234a6b50
Abbas Abbasov <alladywekjd@gmail.com>
2017-10-14 14:52:54 +0300

    Rewrite test to behaviour style

--- src/test/kotlin/org/alladywek/qsort/QSortTest.kt
+++ src/test/kotlin/org/alladywek/qsort/QSortTest.kt
@@ -1,32 +1,26 @@
 package org.alladywek.qsort
 
-import io.kotlintest.matchers.shouldBe
+import io.kotlintest.matchers.shouldEqual
 import io.kotlintest.properties.Gen
-import io.kotlintest.properties.forAll
-import io.kotlintest.properties.headers
-import io.kotlintest.properties.row
-import io.kotlintest.properties.table
-import io.kotlintest.specs.StringSpec
+import io.kotlintest.specs.BehaviorSpec
 
 
-class QSortTest : StringSpec() {
+class QSortTest : BehaviorSpec() {
 
     init {
 
-        "should sort list of integers" {
-            val instance = QSort()
-            val myTable = table(
-                    headers("inArr", "expectedArr"),
-                    row(mutableListOf(8, 3, 5, 1, 0, 4, 3), mutableListOf(0, 1, 3, 3, 4, 5, 8)),
-                    row(mutableListOf(1), mutableListOf(1)),
-                    row(mutableListOf(), mutableListOf()),
-                    row(mutableListOf(1, 2), mutableListOf(1, 2)),
-                    row(mutableListOf(3, 1, 2), mutableListOf(1, 2, 3))
-            )
-            forAll(myTable) { a, b ->
-                Gen.int()
-                instance.sort(a)
-                a shouldBe b
+        Given("List of unsorted integer numbers") {
+            val list = MutableList(1000) {
+                 Gen.positiveIntegers().generate()
+            }
+            val expected = list.sorted()
+
+            When("I try sort list of integer numbers") {
+                QSort().sort(list)
+
+                Then("It should be sorted") {
+                    list shouldEqual expected
+                }
             }
         }
     }
