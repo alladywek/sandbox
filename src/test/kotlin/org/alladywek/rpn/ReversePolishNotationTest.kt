b30fb6f01314f13ac2c886ef0f589b87b47550be
Abbas Abbasov <alladywekjd@gmail.com>
2017-10-15 23:44:59 +0300

    RPN - throws exception if expression has illegal signs

--- src/test/kotlin/org/alladywek/rpn/ReversePolishNotationTest.kt
+++ src/test/kotlin/org/alladywek/rpn/ReversePolishNotationTest.kt
@@ -2,6 +2,7 @@ package org.alladywek.rpn
 
 import io.kotlintest.matchers.shouldBe
 import io.kotlintest.matchers.shouldEqual
+import io.kotlintest.matchers.shouldThrow
 import io.kotlintest.properties.forAll
 import io.kotlintest.properties.headers
 import io.kotlintest.properties.row
@@ -32,6 +33,22 @@ class ReversePolishNotationTest : FunSpec() {
             )
             forAll(testData) { expression, result ->
                 ReversePolishNotation.from(expression) shouldEqual result
+            }
+        }
+
+        test("ReversePolishNotation.from() throws IllegalArgumentException with message if expression has illegal sign") {
+            val testData = table(
+                    headers("expression", "message"),
+                    row("1 + 2a", "Unexpected sign [2a] in expression [1 + 2a]"),
+                    row("x", "Unexpected sign [x] in expression [x]"),
+                    row("abc + 1", "Unexpected sign [abc] in expression [abc + 1]"),
+                    row("abc + 1 - qwerty", "Unexpected sign [abc] in expression [abc + 1 - qwerty]")
+            )
+            forAll(testData) { expression, message ->
+                val exception = shouldThrow<IllegalArgumentException> {
+                    ReversePolishNotation.from(expression)
+                }
+                exception.message shouldBe message
             }
         }
     }
