8f8650049991b3030cca77df9833689ba835042b
Abbas Abbasov <alladywekjd@gmail.com>
2017-10-16 13:36:31 +0300

    RPN - implement parentheses support

--- src/main/kotlin/org/alladywek/rpn/ReversePolishNotation.kt
+++ src/main/kotlin/org/alladywek/rpn/ReversePolishNotation.kt
@@ -18,6 +18,8 @@ class ReversePolishNotation {
                     it == "-" -> signs.add(Minus())
                     it == "*" -> signs.add(Multiply())
                     it == "/" -> signs.add(Divide())
+                    it == "(" -> signs.add(OpenBracket())
+                    it == ")" -> signs.add(CloseBracket())
                     it.toDoubleOrNull() != null -> signs.add(Number(it.toDouble()))
                     else -> throw IllegalArgumentException("Unexpected sign [$it] in expression [$expression]")
                 }
@@ -31,11 +33,24 @@ class ReversePolishNotation {
             signs.forEach {
                 when (it) {
                     is Number -> result.add(it)
+                    is OpenBracket -> stack.add(OpenBracket())
+                    is CloseBracket -> processCloseBracket(result, stack)
                     is Operation -> distributionOperation(result, stack, it)
                 }
             }
+            validateFinalStack(stack)
             result.addAll(stack.reversed())
             return result.joinToString(" ")
+        }
+
+        private fun processCloseBracket(result: ArrayList<Sign>, stack: ArrayList<Operation>) {
+            if (stack.isNotEmpty() && stack.contains(OpenBracket())) {
+                while (true) {
+                    val stackElement = stack.removeAt(stack.lastIndex)
+                    if (stackElement is OpenBracket) return else result.add(stackElement)
+                }
+            }
+            throw IllegalArgumentException("The expression contains inconsistent parentheses")
         }
 
         private fun distributionOperation(result: ArrayList<Sign>, stack: ArrayList<Operation>, operation: Operation) {
@@ -43,6 +58,12 @@ class ReversePolishNotation {
                 result.add(stack.removeAt(stack.lastIndex))
             }
             stack.add(operation)
+        }
+
+        private fun validateFinalStack(stack: ArrayList<Operation>) {
+            if (stack.contains(OpenBracket())) {
+                throw IllegalArgumentException("The expression contains inconsistent parentheses")
+            }
         }
     }
 }
@@ -84,6 +105,36 @@ class Multiply : Operation(2) {
 
     override fun toString(): String {
         return "*"
+    }
+}
+
+class OpenBracket : Operation(0) {
+
+    override fun equals(other: Any?): Boolean {
+        return other is OpenBracket
+    }
+
+    override fun toString(): String {
+        return "("
+    }
+
+    override fun hashCode(): Int {
+        return 1
+    }
+}
+
+class CloseBracket : Operation(0) {
+
+    override fun equals(other: Any?): Boolean {
+        return other is CloseBracket
+    }
+
+    override fun toString(): String {
+        return ")"
+    }
+
+    override fun hashCode(): Int {
+        return 2
     }
 }
 
--- src/test/kotlin/org/alladywek/rpn/ReversePolishNotationTest.kt
+++ src/test/kotlin/org/alladywek/rpn/ReversePolishNotationTest.kt
@@ -13,12 +13,12 @@ class ReversePolishNotationTest : FunSpec() {
 
     init {
 
-        test("ReversePolishNotation.from() should return empty string if expression is blank") {
+        test("ReversePolishNotation.from() should return empty string if the expression is blank") {
             ReversePolishNotation.from("") shouldBe ""
             ReversePolishNotation.from("    ") shouldBe ""
         }
 
-        test("ReversePolishNotation.from() throws IllegalArgumentException with message if expression has illegal sign") {
+        test("ReversePolishNotation.from() throws IllegalArgumentException with message if the expression has illegal sign") {
             val testData = table(
                     headers("expression", "message"),
                     row("1 + 2a", "Unexpected sign [2a] in expression [1 + 2a]"),
@@ -36,7 +36,7 @@ class ReversePolishNotationTest : FunSpec() {
             }
         }
 
-        test("ReversePolishNotation.from() should return RPN for expression with addition and division") {
+        test("ReversePolishNotation.from() should return RPN for the expression with addition and division") {
             val testData = table(
                     headers("expression", "result"),
                     row("1 + 2", "1 2 +"),
@@ -54,7 +54,7 @@ class ReversePolishNotationTest : FunSpec() {
             }
         }
 
-        test("ReversePolishNotation.from() should return RPN from expression with multiplication and division") {
+        test("ReversePolishNotation.from() should return RPN from the expression with multiplication and division") {
             val testData = table(
                     headers("expression", "result"),
                     row("1 * 2", "1 2 *"),
@@ -67,13 +67,47 @@ class ReversePolishNotationTest : FunSpec() {
             }
         }
 
-        test("ReversePolishNotation.from() should return RPN for complex expression") {
+        test("ReversePolishNotation.from() should return RPN for the complex expression") {
             val testData = table(
                     headers("expression", "result"),
                     row("1 * 2 + 2.2", "1 2 * 2.2 +"),
                     row("1 / 2 - 5", "1 2 / 5 -"),
                     row("4 - 1.0 * 2.55", "4 1 2.55 * -"),
+                    row("1 + 0.99 / 2", "1 0.99 2 / +"),
+                    row("1 + 0.99 / 2", "1 0.99 2 / +"),
+                    row("( 1 + 0.99 ) / 2 + 0.8", "1 0.99 + 2 / 0.8 +"),
+                    row("( 1 + 0.99 ) / ( 2 - 0.8 )", "1 0.99 + 2 0.8 - /"),
                     row("1 + 0.99 / 2", "1 0.99 2 / +")
+            )
+            forAll(testData) { expression, result ->
+                ReversePolishNotation.from(expression) shouldEqual result
+            }
+        }
+
+        test("ReversePolishNotation.from() throws IllegalArgumentException with message if the expression contains inconsistent parentheses") {
+            val testData = table(
+                    headers("expression"),
+                    row("( 1 + ( 2 + 3 )"),
+                    row("( 1 + ( 2 + 3"),
+                    row("1 + 2 + 3 )"),
+                    row("1 + 2 ) + 3 )"),
+                    row("( 1 + ) 2 + 3 )")
+            )
+            forAll(testData) { expression ->
+                val exception = shouldThrow<IllegalArgumentException> {
+                    ReversePolishNotation.from(expression)
+                }
+                exception.message shouldBe "The expression contains inconsistent parentheses"
+            }
+        }
+
+        test("ReversePolishNotation.from() should return RPN for the expression with parentheses") {
+            val testData = table(
+                    headers("expression", "result"),
+                    row("( 2 + 2.2 )", "2 2.2 +"),
+                    row("( 1 / 2 ) - 5", "1 2 / 5 -"),
+                    row("( 4 - 1.0 ) * 2.55", "4 1 - 2.55 *"),
+                    row("1 / ( 2 - 5 )", "1 2 5 - /")
             )
             forAll(testData) { expression, result ->
                 ReversePolishNotation.from(expression) shouldEqual result
