273be5d1ab527d663ff084e4f45c53d961776a77
Abbas Abbasov <alladywekjd@gmail.com>
2017-10-28 02:52:48 +0300

    Sudoku - add type safe builder

--- src/main/kotlin/org/alladywek/sudoku/Sudoku.kt
+++ src/main/kotlin/org/alladywek/sudoku/Sudoku.kt
@@ -1,5 +1,45 @@
 package org.alladywek.sudoku
 
+fun sudoku(init: Sudoku.() -> Unit): Sudoku {
+    val sudoku = Sudoku()
+    sudoku.init()
+    return sudoku
+}
+
 class Sudoku {
 
+    private val lines = arrayListOf<Line>()
+
+    fun isSolved(): Boolean {
+        return false
+    }
+
+    fun line(init: Line.() -> Unit): Line {
+        val line = Line()
+        line.init()
+        lines.add(line)
+        return line
+    }
+
+    fun isEmpty(): Boolean {
+        return lines.isEmpty() || lines.all { it.isEmpty() }
+    }
+}
+
+class Line {
+
+    private val cells = arrayListOf<Cell>()
+
+    fun cell(cellValue: Int = 0) {
+        cells.add(Cell(cellValue))
+    }
+
+    fun isEmpty(): Boolean {
+        return cells.isEmpty() || cells.all { it.isEmpty() }
+    }
+}
+
+class Cell(private var value: Int) {
+
+    fun isEmpty() = value == 0
 }
--- src/test/kotlin/org/alladywek/sudoku/SudokuTest.kt
+++ src/test/kotlin/org/alladywek/sudoku/SudokuTest.kt
@@ -1,12 +1,22 @@
 package org.alladywek.sudoku
 
 import io.kotlintest.specs.FeatureSpec
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertTrue
 
 class SudokuTest : FeatureSpec() {
 
     init {
-        scenario("") {
+        feature("Sudoku builder") {
+            scenario("isEmpty() should return true if building empty sudoku") {
+                assertTrue(sudoku { }.isEmpty())
+            }
+        }
 
+        feature("Sudoku resolver") {
+            scenario("isResolved() should return false after initialization of Sudoku class without parameters") {
+                assertFalse(Sudoku().isSolved())
+            }
         }
     }
 }
