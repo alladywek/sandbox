ab5d19c1dd40cad23119d1e0c233ca419e5b24e1
Abbas Abbasov <>
2019-05-12 00:17:07 +0300

    challenge4: refactor

--- coding-challenges-kotlin/src/main/kotlin/challange4/SortVouchers.kt
+++ coding-challenges-kotlin/src/main/kotlin/challange4/SortVouchers.kt
@@ -3,35 +3,27 @@ package challange4
 fun sortVouchers(vouchers: String): Result<ValidationError, String> {
     if (vouchers.isBlank())
         return Result.Error(ValidationError.VouchersStringIsBlankOrEmpty())
-    val (current, notCurrent) = vouchers
+    val (current, other) = vouchers
             .split(",")
             .map(String::buildVoucherFromString)
             .partition { it.status.current }
-    val sortedCurrent = current.sortedWith(compareBy(
-            { it.endDate },
-            { it.status },
-            { it.id }
-    )).joinToString(",")
-    val sortedNotCurrent = notCurrent.sortedWith(
-            compareByDescending<Voucher> { it.endDate }
-                    .thenBy { it.status }
-                    .thenBy { it.id }
-    ).joinToString(",")
-    val delimiter = if (sortedCurrent.isEmpty().and(sortedNotCurrent.isEmpty())) "" else ","
-
-    return Result.Success("$sortedCurrent$delimiter$sortedNotCurrent")
+    val sortedCurrent = current.sortedWith(compareBy({ it.endDate }, { it.status }, { it.id }))
+    val sortedOther = other.sortedWith(
+            compareByDescending<Voucher> { it.endDate }.thenComparing(compareBy({ it.status }, { it.id }))
+    )
+    return Result.Success((sortedCurrent + sortedOther).joinToString(","))
 }
 
 private fun String.buildVoucherFromString(): Voucher {
     val (date, status, id) = this.split(":")
-    return Voucher(date.toLong(), Status.valueOf(status), id)
+    return Voucher(date.toLong(), VoucherStatus.valueOf(status), id)
 }
 
-private class Voucher(val endDate: Long, val status: Status, val id: String) {
+private class Voucher(val endDate: Long, val status: VoucherStatus, val id: String) {
     override fun toString(): String = "$endDate:$status:$id"
 }
 
-enum class Status(val current: Boolean) {
+private enum class VoucherStatus(val current: Boolean) {
     Activated(true),
     Available(true),
     Redeemed(false),
