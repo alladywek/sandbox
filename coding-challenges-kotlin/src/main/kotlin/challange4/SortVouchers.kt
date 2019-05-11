7c9da99b52420719625b3ce1006ce84cdd1a11df
Abbas Abbasov <>
2019-05-11 20:12:24 +0300

    add validation if vouchers string is empty or blank

--- coding-challenges-kotlin/src/main/kotlin/challange4/SortVouchers.kt
+++ coding-challenges-kotlin/src/main/kotlin/challange4/SortVouchers.kt
@@ -1,12 +1,16 @@
 package challange4
 
 fun sortVouchers(vouchers: String): Result<ValidationError, String> {
+    if (vouchers.isBlank())
+        return Result.Error(ValidationError.VouchersStringIsBlankOrEmpty())
     return Result.Success(vouchers)
 }
 
 sealed class Result<out E, out V> {
-    data class Success<out V>(val value: V): Result<Nothing, V>()
-    data class Error<out E>(val value: E): Result<E, Nothing>()
+    data class Success<out V>(val value: V) : Result<Nothing, V>()
+    data class Error<out E>(val value: E) : Result<E, Nothing>()
 }
 
-sealed class ValidationError(msg: String): Exception(msg)
+sealed class ValidationError : Exception() {
+    class VouchersStringIsBlankOrEmpty : ValidationError()
+}
--- coding-challenges-kotlin/src/test/kotlin/challange4/SortVouchersTest.kt
+++ coding-challenges-kotlin/src/test/kotlin/challange4/SortVouchersTest.kt
@@ -1,22 +1,39 @@
 package challange4
 
+import challange4.Result.Error
 import challange4.Result.Success
 import org.junit.jupiter.api.DisplayName
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
 import strikt.api.expectThat
 import strikt.assertions.isA
 import strikt.assertions.isEqualTo
 
 class SortVouchersTest {
 
-    @Test
-    @DisplayName("Expect that sortVouchers() returns the same result as input if input data is correct")
-    fun test1() {
-        val input = "input"
+    @ParameterizedTest
+    @ValueSource(strings = [
+        "190111:Activated:ffff,190111:Available:cccc,190112:Activated:bbbb,190112:Available:aaaa",
+        "190111:Activated:ffff,190111:Available:cccc,190112:Activated:bbbb,190112:Available:aaaa,190110:Redeemed:dddd,190110:Expired:eeee",
+        "190111:Available:cccc"
+    ])
+    @DisplayName("sortVouchers() returns the same result as input if input data is correct")
+    fun test1(input: String) {
         val result = sortVouchers(input)
         expectThat(result)
                 .isA<Success<String>>()
                 .get { value }
                 .isEqualTo(input)
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = ["", " ", "   ", "\t", "\n"])
+    @DisplayName("sortVouchers() returns validation error if input has no data")
+    fun test2(input: String) {
+        val result = sortVouchers(input)
+        expectThat(result)
+                .isA<Error<ValidationError>>()
+                .get { value }
+                .isA<ValidationError.VouchersStringIsBlankOrEmpty>()
     }
 }
