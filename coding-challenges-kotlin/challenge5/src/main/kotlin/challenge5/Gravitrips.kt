59337f0be23d0bbc513086ed4bf04316dfa82e0d
Abbas Abbasov <>
2019-05-19 03:32:25 +0300

    challenge5: refactoring

--- coding-challenges-kotlin/challenge5/src/main/kotlin/challenge5/Gravitrips.kt
+++ coding-challenges-kotlin/challenge5/src/main/kotlin/challenge5/Gravitrips.kt
@@ -1,122 +1,39 @@
 package challenge5
 
-import challenge5.Values.NOTHING
-import challenge5.Values.RED
-import challenge5.Values.RED_LAST
-import challenge5.Values.YELLOW
-import challenge5.Values.YELLOW_LAST
-
 fun getGridStatus(fields: List<String>): String {
-    return translateToMatrix(fields)
-            .checkWinnerByHorizontal()
-            .checkWinnerByVertical()
-            .checkWinnerByDiagonal()
-            .checkDrawResult()
-            .checkWhoNext()
-            .status
+    val grid = Grid(fields.map(String::toList))
+    return when {
+        grid.containsOnly('.') -> "Red plays next"
+        grid.hasSequenceOf(4, listOf('r', 'R')) -> "Red wins"
+        grid.hasSequenceOf(4, listOf('y', 'Y')) -> "Yellow wins"
+        'R' in grid -> "Yellow plays next"
+        'Y' in grid -> "Red plays next"
+        else -> "Draw"
+    }
 }
 
-private fun translateToMatrix(fields: List<String>): Result {
-    val matrix = Matrix(fields.size, fields.first().length)
-    fields.forEachIndexed { row, line ->
-        line.forEachIndexed { column, char ->
-            matrix[row, column] = when (char) {
-                'r' -> RED.value
-                'R' -> RED_LAST.value
-                'y' -> YELLOW.value
-                'Y' -> YELLOW_LAST.value
-                else -> 0
-            }
+private fun Grid.hasSequenceOf(length: Int, Chars: List<Char>): Boolean {
+    val hasSequence: (List<Char>) -> Boolean = { chars -> chars.windowed(length).any { it.all(Chars::contains) } }
+    return (rows + columns + diagonalsWithLength(length)).any(hasSequence)
+}
+
+private class Grid(val rows: List<List<Char>>) {
+
+    val columns: List<List<Char>> = 0.until(rows[0].size).map { columnIndex -> rows.map { it[columnIndex] } }
+}
+
+private fun Grid.containsOnly(char: Char): Boolean = rows.all { row -> row.all { it == char } }
+private operator fun Grid.contains(char: Char): Boolean = rows.any { row -> row.any { it == char } }
+
+private fun Grid.diagonalsWithLength(length: Int): List<List<Char>> {
+    val diagonals = mutableListOf<List<Char>>()
+    (0..rows.size - length).forEach { rowIndex ->
+        (0..rows[0].size - length).forEach { columnIndex ->
+            diagonals.add((0 until length).map { shift -> rows[rowIndex + shift][columnIndex + shift] })
+        }
+        ((length - 1) until rows[0].size).forEach { columnIndex ->
+            diagonals.add((0 until length).map { shift -> rows[rowIndex + shift][columnIndex - shift] })
         }
     }
-    return Result(matrix)
+    return diagonals
 }
-
-private fun Result.checkDrawResult(): Result {
-    return if (!statusDetermined && NOTHING.value !in matrix) Result(this.matrix, "Draw") else this
-}
-
-private fun Result.checkWinnerByHorizontal(): Result {
-    if (statusDetermined) return this
-    val variants = matrix.rows().flatMap { row -> row.windowed(4) }
-    return when {
-        variants.any { it.sum() == 41 } -> Result(matrix, "Red wins")
-        variants.any { it.sum() == 401 } -> Result(matrix, "Yellow wins")
-        else -> this
-    }
-}
-
-private fun Result.checkWinnerByVertical(): Result {
-    if (statusDetermined) return this
-    val variants = matrix.columns().flatMap { column -> column.windowed(4) }
-    return when {
-        variants.any { it.sum() == 41 } -> Result(matrix, "Red wins")
-        variants.any { it.sum() == 401 } -> Result(matrix, "Yellow wins")
-        else -> this
-    }
-}
-
-private fun Result.checkWinnerByDiagonal(): Result {
-    if (statusDetermined) return this
-    val variants = matrix.diagonals().flatMap { diagonal -> diagonal.windowed(4) }
-    return when {
-        variants.any { it.sum() == 41 } -> Result(matrix, "Red wins")
-        variants.any { it.sum() == 401 } -> Result(matrix, "Yellow wins")
-        else -> this
-    }
-}
-
-private fun Result.checkWhoNext(): Result = when {
-    statusDetermined -> this
-    matrix.contains(RED_LAST.value) -> Result(matrix, "Yellow plays next")
-    else -> Result(matrix, "Red plays next")
-}
-
-private class Result(val matrix: Matrix, val status: String = "") {
-    val statusDetermined: Boolean = status.isNotEmpty()
-}
-
-private class Matrix(private val rows: Int, private val columns: Int) {
-    private val src: Array<Int> = Array(rows * columns) { NOTHING.value }
-    private val srcTransparent: Array<Int> = Array(rows * columns) { NOTHING.value }
-    operator fun Array<Int>.get(row: Int, column: Int): Int = this[columns * row + column]
-    operator fun set(row: Int, column: Int, value: Int) {
-        src[columns * row + column] = value
-        srcTransparent[rows * column + row] = value
-    }
-
-    operator fun contains(value: Int): Boolean = value in src
-    fun rows(): List<List<Int>> = src.toList().windowed(columns, columns)
-    fun columns(): List<List<Int>> = srcTransparent.toList().windowed(rows, rows)
-    fun diagonals(): List<List<Int>> {
-        val diagonalsH = columns - 4
-        val diagonalsR = rows - 4
-        val result = mutableListOf<List<Int>>()
-        (0..diagonalsH).forEach { column ->
-            (0..diagonalsR).forEach { row ->
-                result.add(listOf(
-                        src[row, column],
-                        src[row + 1, column + 1],
-                        src[row + 2, column + 2],
-                        src[row + 3, column + 3]
-                ))
-                result.add(listOf(
-                        src[row, column + 3],
-                        src[row + 1, column + 2],
-                        src[row + 2, column + 1],
-                        src[row + 3, column]
-                ))
-            }
-        }
-        return result
-    }
-}
-
-private enum class Values(val value: Int) {
-    NOTHING(0),
-    RED(10),
-    RED_LAST(11),
-    YELLOW(100),
-    YELLOW_LAST(101)
-}
-
--- coding-challenges-kotlin/challenge5/src/test/kotlin/challenge5/GravitripsTest.kt
+++ coding-challenges-kotlin/challenge5/src/test/kotlin/challenge5/GravitripsTest.kt
@@ -8,106 +8,157 @@ import strikt.assertions.isEqualTo
 class GravitripsTest {
 
     @Test
-    @DisplayName("getGridStatus() should return correct status")
+    @DisplayName("getGridStatus() should return status 'Red plays next' if grid is empty")
     fun test1() {
+        val inputFields = listOf(
+                listOf(
+                        ".......",
+                        ".......",
+                        ".......",
+                        ".......",
+                        ".......",
+                        "......."),
+                listOf(
+                        ".......",
+                        ".......",
+                        ".......")
+        )
         expect {
-            testData.forEach { (inputFields, expectedStatus) ->
-                that(getGridStatus(inputFields)).isEqualTo(expectedStatus)
-            }
+            that(getGridStatus(inputFields[0])).isEqualTo("Red plays next")
+            that(getGridStatus(inputFields[1])).isEqualTo("Red plays next")
+        }
+    }
+
+    @Test
+    @DisplayName("getGridStatus() should return status 'Red wins' or 'Yellow wins' when player uses DIAGONAL strategy")
+    fun test2() {
+        val inputFields = listOf(
+                listOf(
+                        ".......",
+                        ".......",
+                        "....r..",
+                        "...ry..",
+                        "..Ryr..",
+                        ".ryyyr."),
+                listOf(
+                        ".......",
+                        ".......",
+                        "..r....",
+                        "...ry..",
+                        "..yyr..",
+                        ".ryyyR."),
+                listOf(
+                        ".......",
+                        ".......",
+                        "...y...",
+                        "...ry..",
+                        "...ryy.",
+                        "...rrrY"),
+                listOf(
+                        ".......",
+                        ".......",
+                        "....ry.",
+                        "...rY..",
+                        "...yrr.",
+                        "..yrrry")
+        )
+        expect {
+            that(getGridStatus(inputFields[0])).isEqualTo("Red wins")
+            that(getGridStatus(inputFields[1])).isEqualTo("Red wins")
+            that(getGridStatus(inputFields[2])).isEqualTo("Yellow wins")
+            that(getGridStatus(inputFields[3])).isEqualTo("Yellow wins")
+        }
+    }
+
+    @Test
+    @DisplayName("getGridStatus() should return status 'Red wins' or 'Yellow wins' when player uses HORIZONTAL strategy")
+    fun test3() {
+        val inputFields = listOf(
+                listOf(
+                        ".......",
+                        ".......",
+                        ".yyyY..",
+                        "...ry..",
+                        "...ryr.",
+                        "...rrry"),
+                listOf(
+                        ".......",
+                        ".......",
+                        "..y.y..",
+                        "...ry..",
+                        "...ryy.",
+                        "..Rrrry")
+        )
+        expect {
+            that(getGridStatus(inputFields[0])).isEqualTo("Yellow wins")
+            that(getGridStatus(inputFields[1])).isEqualTo("Red wins")
+        }
+    }
+
+    @Test
+    @DisplayName("getGridStatus() should return status 'Red wins' or 'Yellow wins' when player uses VERTICAL strategy")
+    fun test4() {
+        val inputFields = listOf(
+                listOf(
+                        ".......",
+                        ".......",
+                        ".R.....",
+                        ".r.....",
+                        ".ry....",
+                        ".ryyy.."),
+                listOf(
+                        "rrryyyr",
+                        "ryyrrry",
+                        "yrryyyr",
+                        "yyyrrry",
+                        "Yrryyyr",
+                        "yyyrrry")
+        )
+        expect {
+            that(getGridStatus(inputFields[0])).isEqualTo("Red wins")
+            that(getGridStatus(inputFields[1])).isEqualTo("Yellow wins")
+        }
+    }
+
+    @Test
+    @DisplayName("getGridStatus() should return status 'Draw' if grid doesn't have free cells")
+    fun test5() {
+        val inputFields = listOf(
+                listOf(
+                        "rrryyyr",
+                        "yyyrrry",
+                        "rrryyyr",
+                        "yyyrrry",
+                        "rrryyyr",
+                        "yyyrrry")
+        )
+        expect {
+            that(getGridStatus(inputFields[0])).isEqualTo("Draw")
+        }
+    }
+
+    @Test
+    @DisplayName("getGridStatus() should return status 'Red plays next' or 'Yellow plays next'")
+    fun test6() {
+        val inputFields = listOf(
+                listOf(
+                        ".......",
+                        ".......",
+                        ".......",
+                        "...ry..",
+                        "...ryy.",
+                        "..yrrrY"),
+                listOf(
+                        ".......",
+                        ".......",
+                        ".......",
+                        "...ry..",
+                        "..Rryy.",
+                        "..yrrry")
+        )
+        expect {
+            that(getGridStatus(inputFields[0])).isEqualTo("Red plays next")
+            that(getGridStatus(inputFields[1])).isEqualTo("Yellow plays next")
         }
     }
 }
-
-private val regexFields = "\"[.RYry]+\"".toRegex()
-private val regexStatus = "'(.+)'".toRegex()
-val testData = arrayOf(
-        """
-            ".......",
-            ".......",
-            ".R.....",
-            ".r.....",
-            ".ry....",
-            ".ryyy.." | 'Red wins'
-        """,
-        """
-            ".......",
-            ".......",
-            ".......",
-            ".yy....",
-            ".rrRr..",
-            ".ryyy.." | 'Red wins'
-        """,
-        """
-            ".......",
-            ".......",
-            "....r..",
-            "...ry..",
-            "..Ryr..",
-            ".ryyyr." | 'Red wins'
-        """,
-        """
-            ".......",
-            ".......",
-            "...y...",
-            "...ry..",
-            "...ryy.",
-            "...rrrY" | 'Yellow wins'
-        """,
-        """
-            ".......",
-            ".......",
-            ".yyyY..",
-            "...ry..",
-            "...ryr.",
-            "...rrry" | 'Yellow wins'
-        """,
-        """
-            ".......",
-            ".......",
-            ".y..y..",
-            "...ry..",
-            "...ryy.",
-            "..Rrrry" | 'Red wins'
-        """,
-        """
-            ".......",
-            ".......",
-            ".......",
-            "...ry..",
-            "...ryy.",
-            "..yrrrY" | 'Red plays next'
-        """,
-        """
-            ".......",
-            ".......",
-            ".......",
-            "...ry..",
-            "..Rryy.",
-            "..yrrry" | 'Yellow plays next'
-        """,
-        """
-            "rrryyyr",
-            "yyyrrry",
-            "rrryyyr",
-            "yyyrrry",
-            "rrryyyr",
-            "yyyrrry" | 'Draw'
-        """,
-        """
-            "rrryyyr",
-            "yyyrrry",
-            "ryryyyr",
-            "yyyrrry",
-            "rrryyyr",
-            "yyyrYry" | 'Yellow wins'
-        """,
-        """
-            "rrrRyyr",
-            "yyyrrry",
-            "ryryyyr",
-            "yyyrrry",
-            "rrryyyr",
-            "yyyrrry" | 'Red wins'
-        """
-).map { regexFields.findAll(it).map { it.value.removeSurrounding("\"") }.toList() to
-        regexStatus.find(it)!!.value.removeSurrounding("'") }
